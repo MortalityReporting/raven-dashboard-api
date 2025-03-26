055c8f3f4dd6f2c8aa158977ac3e89a79192253a
ptassev3 <plamentassev@gmail.com>
2025-03-26 17:02:06 -0400

    improved the code to call a function only once

--- api/routers/passthrough.py
+++ api/routers/passthrough.py
@@ -4,12 +4,17 @@ from httpx import AsyncClient
 from api.utils.setup import set_up
 
 router = APIRouter()
+import_url = "http://localhost:8080"
+config = set_up()
+import_url = config.get("UPLOAD_FILE_PASSTHROUGH_URL")
+
 
 @router.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
 async def pass_through(path: str, request: Request):
     client = AsyncClient()
-    config = set_up()
-    url = f"{config['UPLOAD_FILE_PASSTHROUGH_URL']}/{path}"
+    # config = set_up()
+    # import_url = f"{config['UPLOAD_FILE_PASSTHROUGH_URL']}/{path}"
+    url = f"{import_url}/{path}"
     
     req = client.build_request(
         request.method, 
